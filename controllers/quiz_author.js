ac11d49841ac36f653a574edbe1dd373decdbf2a
Magdalena Báscones <mgbascones@gmail.com>
2015-07-29 00:04:47 +0200

    Despliegue en Heroku

---
+++ controllers/quiz_author.js
@@ -0,0 +1,5 @@
+// GET /authors
+// En otro controlador ya que es ajeno al ámbito de la aplicación
+exports.authors = function(req, res) {
+   res.render('authors', {autor: 'Magdalena Báscones Carrillo', foto: '<img src="/images//mhada.png" alt="autor" height="80" width="60">'});
+ };
