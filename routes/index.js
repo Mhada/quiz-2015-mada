8d4628b62ea47eac830a21232762df2b798f358a
Magdalena Báscones <mgbascones@gmail.com>
2015-07-31 13:32:53 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,20 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var quizAuthor = require('../controllers/quiz_author');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-// router.get('/quizes/question', quizController.question);
-// router.get('/quizes/answer', quizController.answer); 
+
+// Definición de ruta para autores
 router.get('/authors', quizAuthor.authors); 
 
 module.exports = router;
